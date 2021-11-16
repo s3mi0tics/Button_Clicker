da4c291d1c2968e3f50f098748bae424ade62b39
Colby Kauk <colby.kauk@gmail.com>
2021-11-16 00:09:12 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function remove(element){
+    element.remove()
+}
+function change(){
+    document.getElementById("logIn").innerHTML="Log Out"
+}
+// function change() // no ';' here
+// {
+//     var elem = document.getElementById("logIn");
+//     if (elem.innerHTML=="Log In") elem.innerHTML = "Log Out";
+//     else elem.innerHTML = "Log In";
+// }
+
